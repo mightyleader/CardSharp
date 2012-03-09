e60f90b8a5e0f889524edf82fd4c7b66124a2764
Robert <robstearn@me.com>
2012-03-09 23:08:56 +0000

    Changes to comments

--- CardSharp/RSViewController.h
+++ CardSharp/RSViewController.h
@@ -3,7 +3,7 @@
 //  CardSharp
 //
 //  Created by Robert Stearn on 03.03.12.
-//  Copyright (c) 2012 Apple UK Retail. All rights reserved.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
